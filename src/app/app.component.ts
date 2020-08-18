ef15b387a355b922f81cae67ee0f72659409532a
mercesolis <mersolis@live.com>
2020-08-17 21:50:34 -0500

    add todo item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Todos';
   todoList: any[] = [];
+  todoTitle: string;
 
   ngOnInit(): void {
+    this.todoTitle = '';
     this.todoList = [
       { title: 'Install Angular CLI', isDone: false}
     ];
+  }
+
+  // adds a todo to our list
+  addTodo():void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+    
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
   }
 
   deleteTodo(todo: any): void {
